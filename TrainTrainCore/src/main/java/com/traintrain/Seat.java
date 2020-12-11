e7ab0176df29b62717c1f65607589e4f56881750
nicolas coquelet <nicolas.coquelet@tryane.com>
2020-12-12 00:55:29 +0100

    clean up, seat is now immuable

--- TrainTrainCore/src/main/java/com/traintrain/Seat.java
+++ TrainTrainCore/src/main/java/com/traintrain/Seat.java
@@ -1,18 +1,20 @@
 package com.traintrain;
 
+import java.util.Objects;
+
 public class Seat {
     private String coachName;
-    private int seatNumber;
-    private String bookingRef;
+    private int    seatNumber;
+    private String bookingReference;
 
     public Seat(String coach, int seatNumber) {
         this(coach, seatNumber, "");
     }
 
-    public Seat(String coachName, int seatNumber, String bookingRef) {
+    public Seat(String coachName, int seatNumber, String bookingReference) {
         this.coachName = coachName;
         this.seatNumber = seatNumber;
-        this.bookingRef = bookingRef;
+        this.bookingReference = bookingReference;
     }
 
     public String getCoachName() {
@@ -31,12 +33,8 @@ public class Seat {
         this.seatNumber = seatNumber;
     }
 
-    public String getBookingRef() {
-        return bookingRef;
-    }
-
-    public void setBookingRef(String bookingRef) {
-        this.bookingRef = bookingRef;
+    boolean isFree() {
+        return bookingReference.isEmpty();
     }
 
     @Override
@@ -44,43 +42,34 @@ public class Seat {
         return coachName + seatNumber;
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((bookingRef == null) ? 0 : bookingRef.hashCode());
-		result = prime * result + ((coachName == null) ? 0 : coachName.hashCode());
-		result = prime * result + seatNumber;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Seat other = (Seat) obj;
-		if (bookingRef == null) {
-			if (other.bookingRef != null)
-				return false;
-		} else if (!bookingRef.equals(other.bookingRef))
-			return false;
-		if (coachName == null) {
-			if (other.coachName != null)
-				return false;
-		} else if (!coachName.equals(other.coachName))
-			return false;
-		if (seatNumber != other.seatNumber)
-			return false;
-		return true;
-	}
-
-    boolean isFree() {
-        return getBookingRef() == "";
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (bookingReference == null ? 0 : bookingReference.hashCode());
+        result = prime * result + (coachName == null ? 0 : coachName.hashCode());
+        result = prime * result + seatNumber;
+        return result;
     }
-    
-    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seat other = (Seat) obj;
+        if (!Objects.equals(bookingReference, other.bookingReference)) {
+            return false;
+        }
+        if (!Objects.equals(coachName, other.coachName)) {
+            return false;
+        }
+        if (seatNumber != other.seatNumber)
+            return false;
+        return true;
+    }
+
 }

d8a5d1b81987f18bcf96ad770cb82b3990f5ff92
nicolas coquelet <nicolas.coquelet@tryane.com>
2020-12-11 18:25:23 +0100

    Revert "add reservation attempt object"
    This reverts commit 042abc9d948a0d4df0089ee52d9d33aa9589dfcc.

--- TrainTrainCore/src/main/java/com/traintrain/TrainTopology.java
+++ TrainTrainCore/src/main/java/com/traintrain/TrainTopology.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class TrainTopology {
+    private int reservedSeats;
     private List<Seat> seats;
 
     public TrainTopology(List<Seat> seats) {
@@ -29,6 +30,9 @@ public class TrainTopology {
             for (SeatJson seatJson : value.values()) {
                 int seat_number = Integer.parseInt(seatJson.seat_number);
                 seats.add(new Seat(seatJson.coach, seat_number, seatJson.booking_reference));
+                if (!(new Seat(seatJson.coach, seat_number, seatJson.booking_reference).getBookingRef() == "")) {
+                    this.reservedSeats++;
+                }
             }
         }
     }
@@ -45,8 +49,7 @@ public class TrainTopology {
         return this.seats.size();
     }
 
-	protected boolean isReservedSeatsUnderThreshold(int nbSeatRequested) {
-		return (getReservedSeats() + nbSeatRequested) <= Math.floor(ThresholdManager.getMaxRes() * getMaxSeat());
-	}
-
+    public boolean hasLessThanThreshold(int i) {
+        return reservedSeats < i;
+    }
 }
